d0fb82c6354c17b7e52e421cbe7d712a08b17dcf
Aleksandr Voronov <alv@qapint.com>
2014-11-13 10:43:43 +0200

    first commit

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/aleksandrvoronov/Projects/Hybrid/my-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/aleksandrvoronov/Projects/Hybrid/my-app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/aleksandrvoronov/Projects/Hybrid/my-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
